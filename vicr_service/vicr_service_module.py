26666e30380f9c54485a18e6044d951b936410eb
vishals9711 <vishalsharma1907@gmail.com>
2023-02-21 12:12:31 -0800

    Initial commit for readme

--- vicr_service/vicr_service_module.py
+++
@@ -1,8 +0,0 @@
-from vicr_scoring import get_vicr_score_from_service
-
-class VICRService:
-    def __init__(self, vicr_service):
-        self.vicr_service = vicr_service
-    
-    def getVICRScore(self):
-        get_vicr_score_from_service(self.video_id)
