1bd957673bce5a89ca1a2439fdb4b35391a75c61
sanjay417 <miranisanjay417@gmail.com>
2024-09-14 14:43:23 -0700

    fixing custom_logger.py

--- web_server_module/custom_logger.py
+++ web_server_module/custom_logger.py
@@ -3,30 +3,38 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 import os
 
+
 def create_daily_rotating_log(path):
     """
     Creates a daily rotating log
     """
     logger = logging.getLogger("Daily Rotating Log")
     logger.setLevel(logging.INFO)
-    
+
     # Add a timed rotating handler
     handler = TimedRotatingFileHandler(path, when="MIDNIGHT", interval=1, backupCount=5)
     return handler
 
-# Define the path to the log file relative to the parent directory
-log_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'pipeline_logs')
 
-if not os.path.exists(log_directory):
-    os.makedirs(log_directory)
+def setup_logger():
+    """
+    Sets up and returns a configured logger
+    """
+    # Define the path to the log file relative to the parent directory
+    log_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'pipeline_logs')
 
-log_filename = os.path.join(log_directory, 'pipeline_{}.log'.format(datetime.datetime.now().strftime("%Y-%m-%d")))
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
 
-web_server_logger = logging.getLogger('my_logger')
-web_server_logger.setLevel(logging.INFO)
+    log_filename = os.path.join(log_directory, f'pipeline_{datetime.datetime.now().strftime("%Y-%m-%d")}.log')
 
-log_handler = create_daily_rotating_log(log_filename)
-web_server_logger.addHandler(log_handler)
+    logger = logging.getLogger('web_server_logger')
+    logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-log_handler.setFormatter(formatter)
+    log_handler = create_daily_rotating_log(log_filename)
+    logger.addHandler(log_handler)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    log_handler.setFormatter(formatter)
+
+    return logger
